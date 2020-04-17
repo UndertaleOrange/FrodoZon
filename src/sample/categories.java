f759cb9dad1ed5ab7e797031b4e7d160ce1eb193
Omniqube Software <UndertaleOrange@users.noreply.github.com>
2020-04-17 12:26:19 +0200

    Doplnujuci update
    -

---
+++ src/sample/categories.java
@@ -0,0 +1,38 @@
+package sample;
+
+public class categories {
+    int CategoryID;
+    String Nazov, Popis;
+    public categories(int ID, String name, String description) {
+        this.CategoryID = CategoryID;
+        this.Nazov = Nazov;
+        this.Popis = Popis;
+
+    }
+
+    public int getCategoryID() {
+        return CategoryID;
+    }
+
+    public String getNazov() {
+        return Nazov;
+    }
+
+    public String getPopis() {
+        return Popis;
+    }
+
+    public void setPopis(String Popis) {
+        this.Popis = Popis;
+    }
+
+    public void setNazov(String Nazov) {
+        this.Nazov = Nazov;
+    }
+
+    public void setCategoryID(int CategoryID) {
+        this.CategoryID = CategoryID;
+    }
+
+
+}
